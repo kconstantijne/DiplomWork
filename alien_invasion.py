bb1ab208148f49ffc3828365b3ac5b6c1ba29f5e
Constantine <constantijne@gmail.com>
2022-11-07 06:52:03 +0200

    Ship and game launch settings were made, comments were written and the code was refactored

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,41 +5,70 @@ from ship import Ship
 
 
 class AlienInvasion:
+    """Загальній клас, який керую повединкою та ресурсами гри"""
 
     def __init__(self):
+        """Ініціалізує гру та створює ресурси гри"""
+
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        # Запуск гри в повноекранному режимі
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+        # # self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
 
     def _check_events(self):
+        """Реагує на натискання клавіш та миші"""
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
         pygame.display.flip()
 
+    def _check_keydown_events(self, event):
+        """Реагує на натискання клавіші"""
+
+        if event.key == pygame.K_END:
+            sys.exit()
+        elif event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Реагує, коли клавіша не натиснута"""
+
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
     def _update_screen(self):
+        """Оновлюється зображення на екрані та перемикається на новий екран"""
+
+        # Наново перемальовуе екран на кожній ітерації циклу
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
 
     def run_game(self):
+        """Розпочинае головний цикл гри"""
+
         while True:
+            # Слідкуе за поведінкою миші та клавіатури та миші
             self._check_events()
             self.ship.update()
             self._update_screen()
 
 
 if __name__ == '__main__':
+    # Створюе екземпляр гри та запускае гру
     ai = AlienInvasion()
     ai.run_game()
--- ship.py
+++ ship.py
@@ -2,27 +2,39 @@ import pygame
 
 
 class Ship:
+    """Клас для керування кораблем"""
+
     def __init__(self, ai_game):
+        """Ініціалізувати корабель та задати його початкову позицію"""
+
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
+        # Завантажити зображення корабля та отримати його rect
         self.image = pygame.image.load('src/image/space_ship.png')
         self.rect = self.image.get_rect()
+        # Створюється кожний новий корабель по центру екрану
         self.rect.midbottom = self.screen_rect.midbottom
-
+        # Зберегти десяткове значення для позиції корабля по горизонталі
         self.x = float(self.rect.x)
-
+        # Індикатор руху
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
+        """Оновити поточну позицію корабля на основі індикатору руху"""
+
+        # Рух корабля та запобігання виходу його за межі екрану
+        # З правого боку
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-
+        # З лівого боку
         if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
-
+        # Оновити об`ект rect з self.x
         self.rect.x = self.x
 
     def blitme(self):
+        """Намалювати корабель у його поточному розташуванні"""
+
         self.screen.blit(self.image, self.rect)
