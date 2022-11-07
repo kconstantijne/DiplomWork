81e1559a6f4ca253f69c1f7d91e57d94cb6d18c9
Constantine <constantijne@gmail.com>
2022-11-07 03:51:51 +0200

    Made the ship move left and right

---
+++ ship.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class Ship:
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load('src/image/space_ship.png')
+        self.rect = self.image.get_rect()
+
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
